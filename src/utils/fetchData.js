86adeedf7eaa45ad71f188ac416bcf3f30bdb4ea
mshiveshm <2k22.csai.2212494@gmail.com>
2024-11-02 13:00:41 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+    'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': '5924e06b88mshbee06026be02613p15e74fjsn2d5b0a628f94',
+    'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
